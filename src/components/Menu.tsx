83960fa498f1b3722d6bf78eeb881a9a9bd9d35b
gabisreis7 <gabisousareis07@gmail.com>
2024-10-31 16:19:12 -0300

    Commit inicial

---
+++ src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Menu(){
+    return(
+        <nav>
+            <ul>
+                <li><Link href={'/'}>Home</Link></li>
+                <li><Link href={'/produtos'}>Produtos</Link></li>
+                <li><Link href={'/cadastro-produtos'}>Cadastro de Produtos</Link></li>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/components/Rodape.tsx
@@ -0,0 +1,7 @@
+export default function Rodape(){
+    return(
+        <footer>
+            <h1>Gabriela de Sousa Reis - RM558830 e Laura Amadeu Soares - RM556690</h1>
+        </footer>
+    )
+}
