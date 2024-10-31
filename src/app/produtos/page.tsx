2ac97a8b3cac8504aee336d2627a39ae9c47df3d
gabisreis7 <gabisousareis07@gmail.com>
2024-10-31 17:47:56 -0300

    Preenchendo CRUD

---
+++ src/app/produtos/page.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export default function Produto(){
+
+    const[acess, setAcess] = useState(false)
+    const[disable, setDisable] = useState(0)
+
+
+
+    return(
+        <main></main>
+
+    )
+}
